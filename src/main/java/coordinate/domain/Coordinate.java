357022aa4bb7e922909adb4c6738bcfdd581f350
DaejunBae <dpudpu11@gmail.com>
2019-11-29 18:55:09 +0900

    feat: Coordinate.class

---
+++ src/main/java/coordinate/domain/Coordinate.java
@@ -0,0 +1,61 @@
+package coordinate.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class Coordinate {
+    static final String RANGE_EXCEPTION_MESSAGE = "%d~%d 사이만 입력 가능합니다.";
+    private static final Map<Integer, Coordinate> CACHE = new HashMap<>();
+
+    static final int MIN_VALUE = 0;
+    static final int MAX_VALUE = 24;
+
+    static {
+        IntStream.range(MIN_VALUE, MAX_VALUE).forEach(value -> CACHE.put(value, new Coordinate(value)));
+    }
+
+    private final int value;
+
+    private Coordinate(final int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(final int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException(String.format(RANGE_EXCEPTION_MESSAGE, MIN_VALUE, MAX_VALUE));
+        }
+    }
+
+    public static Coordinate of(final int value) {
+        final Coordinate coordinate = CACHE.get(value);
+        return coordinate != null ? coordinate : new Coordinate(value);
+    }
+
+
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Coordinate coordinate = (Coordinate) o;
+        return value == coordinate.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "value=" + value +
+                '}';
+    }
+}
---
+++ src/test/java/coordinate/domain/CoordinateTest.java
@@ -0,0 +1,41 @@
+package coordinate.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static coordinate.domain.Coordinate.MAX_VALUE;
+import static coordinate.domain.Coordinate.MIN_VALUE;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CoordinateTest {
+
+    @Test
+    void 정상_생성_확인() {
+        Coordinate coordinate = Coordinate.of(MAX_VALUE - 1);
+        assertThat(coordinate.value()).isEqualTo(MAX_VALUE - 1);
+    }
+
+    @Test
+    void 캐싱_확인() {
+        Coordinate coordinate1 = Coordinate.of(23);
+        Coordinate coordinate2 = Coordinate.of(23);
+
+        assertThat(coordinate1 == coordinate2).isTrue();
+    }
+
+    @Test
+    void createException_최소값_미만() {
+        String expected = String.format(Coordinate.RANGE_EXCEPTION_MESSAGE, MIN_VALUE, MAX_VALUE);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Coordinate.of(MIN_VALUE - 1));
+
+        assertThat(exception.getMessage()).isEqualTo(expected);
+    }
+
+    @Test
+    void createException_최대값_미만() {
+        String expected = String.format(Coordinate.RANGE_EXCEPTION_MESSAGE, MIN_VALUE, MAX_VALUE);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Coordinate.of(MAX_VALUE + 1));
+
+        assertThat(exception.getMessage()).isEqualTo(expected);
+    }
+}
