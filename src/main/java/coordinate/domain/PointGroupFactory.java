6745e0728437641edda0d48e8c4bfc58fb8928d0
DaejunBae <dpudpu11@gmail.com>
2019-12-01 02:46:35 +0900

    feat: PointGroupFactory.class

---
+++ src/main/java/coordinate/domain/PointGroupFactory.java
@@ -0,0 +1,36 @@
+package coordinate.domain;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PointGroupFactory {
+    static final String NOT_VALID_INPUT_EXCEPTION_MESSAGE = "유효한 입력이 아닙니다. ex) (10,10)-(10,10)-(20,8)";
+    private static final String REGEX = "(\\([0-9]{1,2},[0-9]{1,2}\\)-)+\\([0-9]{1,2},[0-9]{1,2}\\)";
+    private static final Pattern VALID_INPUT_PATTERN = Pattern.compile(REGEX);
+    private static final String DELIMITER_POINT = "-";
+    private static final String DELIMITER_COORDINATE = ",";
+
+    public PointGroup create(final String inputPoints) {
+        validateInput(inputPoints);
+
+        final List<Point> points = Stream.of(inputPoints.split(DELIMITER_POINT))
+                .map(s -> s.substring(1, s.length() - 1)) // (1,2) 에서 () 제거
+                .map(point -> { // 1,2 에서 ,제거
+                    int index = point.indexOf(DELIMITER_COORDINATE);
+                    int x = Integer.parseInt(point.substring(0, index));
+                    int y = Integer.parseInt(point.substring(index + 1));
+                    return Point.of(x, y);
+                })
+                .collect(Collectors.toList());
+
+        return PointGroup.of(points);
+    }
+
+    private void validateInput(final String inputPoints) {
+        if (!VALID_INPUT_PATTERN.matcher(inputPoints).matches()) {
+            throw new IllegalArgumentException(NOT_VALID_INPUT_EXCEPTION_MESSAGE);
+        }
+    }
+}
---
+++ src/test/java/coordinate/domain/PointGroupFactoryTest.java
@@ -0,0 +1,37 @@
+package coordinate.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static coordinate.domain.PointGroupFactory.NOT_VALID_INPUT_EXCEPTION_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PointGroupFactoryTest {
+    private final PointGroupFactory factory = new PointGroupFactory();
+
+    @Test
+    void 생성_정상입력_확인() {
+        final PointGroup pointGroup1 = factory.create("(10,10)-(14,15)");
+        final PointGroup pointGroup2 = factory.create("(10,10)-(14,15)-(11,19)");
+
+        assertThat(pointGroup1.matchSize(2)).isTrue();
+        assertThat(pointGroup2.matchSize(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "10,10)-(14,15)",
+            "(10,10)(14,15)",
+            "(10,10)-(14,15",
+            "(10,10),(14,15)",
+            "10,10-14,15",
+            "[10,10]-[14,15]"
+    })
+    void 생성_잘못_입력_예외처리(final String input) {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> factory.create(input));
+
+        assertThat(exception.getMessage()).isEqualTo(NOT_VALID_INPUT_EXCEPTION_MESSAGE);
+    }
+}
