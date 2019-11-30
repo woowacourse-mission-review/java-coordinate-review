02cbb70b36b5578ed159a61600ce426b3fffe075
DaejunBae <dpudpu11@gmail.com>
2019-11-30 14:19:10 +0900

    refactor: PointGroup.points 컬렉션 변경 Set -> List

--- src/main/java/coordinate/domain/PointGroup.java
+++ src/main/java/coordinate/domain/PointGroup.java
@@ -1,5 +1,6 @@
 package coordinate.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,21 +8,26 @@ import java.util.Set;
 public class PointGroup {
     static final String DUPLICATED_POINT_EXCEPTION_MESSAGE = "Point는 중복될 수 없습니다.";
 
-    final Set<Point> points;
+    final List<Point> points;
 
     private PointGroup(final List<Point> points) {
-        this.points = new HashSet<>(points);
+        this.points = new ArrayList<>(points);
         validateDuplicate(points);
     }
 
     private void validateDuplicate(final List<Point> points) {
-        if (points.size() != size()) {
+        final Set<Point> set = new HashSet<>(points);
+        if (points.size() != set.size()) {
             throw new IllegalArgumentException(DUPLICATED_POINT_EXCEPTION_MESSAGE);
         }
     }
 
     public static PointGroup of(final List<Point> points) {
         return new PointGroup(points);
+    }
+
+    public Point get(final int index) {
+        return points.get(index);
     }
 
     public int size() {
