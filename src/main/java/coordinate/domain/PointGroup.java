8d6479893c38302c89515613de8f3f4605e690b4
DaejunBae <dpudpu11@gmail.com>
2019-12-01 01:05:21 +0900

    refactor: PointGroup 예외처리 메시지 변경

--- src/main/java/coordinate/domain/PointGroup.java
+++ src/main/java/coordinate/domain/PointGroup.java
@@ -7,8 +7,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class PointGroup {
-    static final String DUPLICATED_POINT_EXCEPTION_MESSAGE = "Point는 중복될 수 없습니다.";
-    static final String EMPTY_EXCEPTION_MESSAGE = "Point가 하나도 없습니다.";
+    static final String DUPLICATED_POINT_EXCEPTION_MESSAGE = "위치가 같은 점(point)이 존재합니다. 점의 위치는 달라야 합니다.";
+    static final String EMPTY_EXCEPTION_MESSAGE = "점(point)가 하나도 없습니다.";
 
     private final List<Point> points;
 
