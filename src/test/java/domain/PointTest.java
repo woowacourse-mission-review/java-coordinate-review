dae0d8c0a63b2ecf0d495543e607fe3ded5af54b
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-12-01 03:12:01 +0900

    test: point와 points test 추가

--- src/test/java/domain/PointTest.java
+++ src/test/java/domain/PointTest.java
@@ -30,4 +30,58 @@ public class PointTest {
         Exception exception = assertThrows(UnavailabeCoordinateException.class, () -> new Point("23,-1"));
         assertThat(exception.getMessage()).isEqualTo(UnavailabeCoordinateException.MESSAGE);
     }
+
+    @Test
+    @DisplayName("점과 점 사이의 거리 구하기")
+    void calculate_distance_of_points() {
+        Point point1 = new Point("0,0");
+        Point point2 = new Point("24,0");
+
+        assertThat(point1.calculateDistance(point2)).isEqualTo(24);
+    }
+
+    @Test
+    @DisplayName("점과 점 사이의 거리 구하기")
+    void calculate_distance_of_points2() {
+        Point point1 = new Point("0,0");
+        Point point2 = new Point("6,11");
+
+        assertThat(point1.calculateDistance(point2)).isEqualTo(Math.sqrt(157));
+    }
+
+    @Test
+    @DisplayName("두 점이 같은 X좌표를 갖는지 확인하기")
+    void same_x_coordinate_only() {
+        Point point1 = new Point("0,5");
+        Point point2 = new Point("0,7");
+
+        assertThat(point1.doesHaveSameXCoordinateOnly(point2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("두 점이 같은 X좌표를 갖는지 확인하기")
+    void same_x_coordinate_only2() {
+        Point point1 = new Point("2,5");
+        Point point2 = new Point("2,7");
+
+        assertThat(point1.doesHaveSameXCoordinateOnly(point2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("두 점이 같은 Y좌표를 갖는지 확인하기")
+    void same_y_coordinate_only() {
+        Point point1 = new Point("1,5");
+        Point point2 = new Point("2,5");
+
+        assertThat(point1.doesHaveSameYCoordinateOnly(point2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("두 점이 같은 Y좌표를 갖는지 확인하기")
+    void same_y_coordinate_only2() {
+        Point point1 = new Point("3,7");
+        Point point2 = new Point("2,7");
+
+        assertThat(point1.doesHaveSameYCoordinateOnly(point2)).isTrue();
+    }
 }
--- src/test/java/domain/PointsTest.java
+++ src/test/java/domain/PointsTest.java
@@ -29,4 +29,29 @@ class PointsTest {
     void fail_points_constructor_test() {
         assertThrows(DuplicatePointsException.class, () -> new Points("(1,2)-(1,2)"));
     }
+
+    @Test
+    @DisplayName("Points 거리구하기")
+    void calculate_distance_test() {
+        Points points = new Points("(0,0)-(6,11)");
+        assertThat(points.calculateDistance(0, 1)).isEqualTo(Math.sqrt(157));
+    }
+
+    @Test
+    @DisplayName("직각인 두 선분의 사이즈 구하기")
+    void find_perpendicular_sides_sizes_test() {
+        Points points = new Points("(0,0)-(4,0)-(0,2)-(4,2)");
+        assertThat(points.findPerpendicularSidesSizes().size()).isEqualTo(2);
+        assertThat(points.findPerpendicularSidesSizes().contains(4.0)).isTrue();
+        assertThat(points.findPerpendicularSidesSizes().contains(2.0)).isTrue();
+    }
+
+    @Test
+    @DisplayName("직각인 두 선분의 사이즈 구하기")
+    void find_perpendicular_sides_sizes_test2() {
+        Points points = new Points("(1,1)-(5,1)-(1,3)-(5,3)");
+        assertThat(points.findPerpendicularSidesSizes().size()).isEqualTo(2);
+        assertThat(points.findPerpendicularSidesSizes().contains(4.0)).isTrue();
+        assertThat(points.findPerpendicularSidesSizes().contains(2.0)).isTrue();
+    }
 }
