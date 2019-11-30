3f5a38f3b30272118f4fae79bb5cd7800dadb4c2
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-11-30 21:38:27 +0900

    feat: 선분 길이 구하기 추가

--- src/test/java/domain/figure/LineTest.java
+++ src/test/java/domain/figure/LineTest.java
@@ -16,4 +16,19 @@ class LineTest {
         assertThat(line.calculateDistanceOrArea()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("선분의 길이 구하기")
+    void calculate_distance2() {
+        Points points = new Points("(0,0)-(4,0)");
+        Figure line = FigureFactory.createFigure(points);
+        assertThat(line.calculateDistanceOrArea()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("선분의 길이 구하기")
+    void calculate_distance3() {
+        Points points = new Points("(0,0)-(2,2)");
+        Figure line = FigureFactory.createFigure(points);
+        assertThat(line.calculateDistanceOrArea()).isEqualTo(Math.sqrt(8));
+    }
 }
