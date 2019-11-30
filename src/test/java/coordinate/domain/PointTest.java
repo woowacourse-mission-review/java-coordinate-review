5a10e93e9f0c4eb3e55ad1de19f0ff98f3e1c6a7
DaejunBae <dpudpu11@gmail.com>
2019-11-30 13:02:43 +0900

    feat: Point.class

---
+++ src/test/java/coordinate/domain/PointTest.java
@@ -0,0 +1,29 @@
+package coordinate.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+class PointTest {
+
+    @Test
+    void 논리적_동치성_검사() {
+        final Point point1 = Point.of(10, 11);
+        final Point point2 = Point.of(10, 11);
+
+        assertThat(point1).isEqualTo(point2);
+        assertThat(point1 == point2).isFalse();
+    }
+
+    @Test
+    void Point_두개_거리() {
+        final Point point1 = Point.of(10, 10);
+        final Point point2 = Point.of(14, 15);
+        final double expected = 6.403124;
+
+        final double actual = point1.distanceTo(point2);
+
+        assertThat(actual).isEqualTo(expected, offset(0.0009));
+    }
+}
