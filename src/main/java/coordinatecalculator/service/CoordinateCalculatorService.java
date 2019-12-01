10c871e1554e0294792293abc4f49a61ecad354f
skygl <jupiter9501@gmail.com>
2019-12-01 17:36:59 +0900

    feat: 좌표 값의 String 을 받아 Result 를 만드는 서비스 클래스 추가

---
+++ src/main/java/coordinatecalculator/service/CoordinateCalculatorService.java
@@ -0,0 +1,21 @@
+package coordinatecalculator.service;
+
+import coordinatecalculator.domain.PointParser;
+import coordinatecalculator.domain.figure.Figure;
+import coordinatecalculator.domain.figure.FigureFactory;
+import coordinatecalculator.domain.figure.Point;
+import coordinatecalculator.domain.result.Result;
+
+import java.util.List;
+
+public class CoordinateCalculatorService {
+
+    private final FigureFactory figureFactory = new FigureFactory();
+    private final PointParser pointParser = new PointParser();
+
+    public Result calculate(String rawInputPoints) {
+        List<Point> points = pointParser.parse(rawInputPoints);
+        Figure figure = figureFactory.createFigure(points);
+        return figure.createResult();
+    }
+}
---
+++ src/test/java/coordinatecalculator/service/CoordinateCalculatorServiceTest.java
@@ -0,0 +1,64 @@
+package coordinatecalculator.service;
+
+import coordinatecalculator.domain.figure.Point;
+import coordinatecalculator.domain.result.Result;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CoordinateCalculatorServiceTest {
+
+    private final CoordinateCalculatorService service = new CoordinateCalculatorService();
+
+    @Test
+    @DisplayName("두 개의 다른 점을 입력한 경우")
+    void calculate_two_different_points() {
+        String input = "(1,2)-(3,4)";
+
+        Result result = service.calculate(input);
+        assertTrue(result.contains(Point.of(1, 2)));
+        assertTrue(result.contains(Point.of(3, 4)));
+    }
+
+    @Test
+    @DisplayName("한 개의 점을 입력한 경우")
+    void calculate_one_point() {
+        String input = "(1,2)";
+
+        assertThrows(IllegalArgumentException.class, () -> service.calculate(input));
+    }
+
+    @Test
+    @DisplayName("4개 초과의 점을 입력한 경우")
+    void calculate_over_4_points() {
+        String input = "(1,1)-(20,10)-(1,10)-(20,10)-(10,20)";
+
+        assertThrows(IllegalArgumentException.class, () -> service.calculate(input));
+    }
+
+    @Test
+    @DisplayName("잘못된 점들의 DELIMITER 를 사용한 경우")
+    void calculate_wrong_point_delimiter_format() {
+        String input = "(1,2)=(3,4)";
+
+        assertThrows(IllegalArgumentException.class, () -> service.calculate(input));
+    }
+
+    @Test
+    @DisplayName("잘못된 좌표들의 DELIMITER 를 사용한 경우")
+    void calculate_wrong_coordinate_delimiter_format() {
+        String input = "(1_2)-(3_4)";
+
+        assertThrows(IllegalArgumentException.class, () -> service.calculate(input));
+    }
+
+    @Test
+    @DisplayName("정수가 아닌 값들을 좌표값으로 입력한 경우")
+    void calculate_not_integer_point_format() {
+        String input = "(one,two)-(three,four)";
+
+        assertThrows(IllegalArgumentException.class, () -> service.calculate(input));
+    }
+}
