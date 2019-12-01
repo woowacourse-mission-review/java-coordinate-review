cca7acb2a061f4ac25878a62b8148faf71be0eac
skygl <jupiter9501@gmail.com>
2019-12-01 19:52:09 +0900

    feat: Triangle 도메인 클래스 추가

---
+++ src/main/java/coordinatecalculator/domain/figure/triangle/Triangle.java
@@ -0,0 +1,74 @@
+package coordinatecalculator.domain.figure.triangle;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import coordinatecalculator.domain.figure.AttributeCreator;
+import coordinatecalculator.domain.figure.Figure;
+import coordinatecalculator.domain.figure.Point;
+import coordinatecalculator.domain.result.Result;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Triangle implements Figure {
+
+    private final List<AttributeCreator> attributeCreators = Lists.newArrayList();
+
+    private final List<Point> points;
+
+    public Triangle(final List<Point> points) {
+        validate(points);
+        this.points = points;
+    }
+
+    private void validate(final List<Point> points) {
+        if (points.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+        if (points.size() != Sets.newHashSet(points).size()) {
+            throw new IllegalArgumentException();
+        }
+        validateSlope(points);
+    }
+
+    private void validateSlope(List<Point> points) {
+        Point point1 = points.get(0);
+        Point point2 = points.get(1);
+        Point point3 = points.get(2);
+
+        int leftHandSide = (point2.yValue() - point1.yValue()) * (point3.xValue() - point1.xValue());
+        int rightHandSide = (point3.yValue() - point1.yValue()) * (point2.xValue() - point1.xValue());
+
+        if (leftHandSide == rightHandSide) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean contains(final Point point) {
+        return points.contains(point);
+    }
+
+    @Override
+    public Result createResult() {
+        List<String> attributesMessages = attributeCreators.stream()
+                .map(attributeCreator -> attributeCreator.create(points))
+                .collect(Collectors.toList());
+
+        return Result.of(points, attributesMessages);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Triangle triangle = (Triangle) o;
+        return points.containsAll(triangle.points) && triangle.points.containsAll(points);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Sets.newHashSet(points));
+    }
+}
---
+++ src/test/java/coordinatecalculator/domain/figure/triangle/TriangleTest.java
@@ -0,0 +1,113 @@
+package coordinatecalculator.domain.figure.triangle;
+
+import com.google.common.collect.Lists;
+import coordinatecalculator.domain.figure.Point;
+import org.assertj.core.util.Sets;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static coordinatecalculator.domain.coordinates.XCoordinate.MAX_X_COORDINATE;
+import static coordinatecalculator.domain.coordinates.XCoordinate.MIN_X_COORDINATE;
+import static coordinatecalculator.domain.coordinates.YCoordinate.MAX_Y_COORDINATE;
+import static coordinatecalculator.domain.coordinates.YCoordinate.MIN_Y_COORDINATE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+
+    @Test
+    @DisplayName("삼각형 생성")
+    void generate_triangle() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point3 = Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE);
+        List<Point> points = Lists.newArrayList(point1, point2, point3);
+
+        assertDoesNotThrow(() -> new Triangle(points));
+    }
+
+    @Test
+    @DisplayName("3개 미만 점들로 삼각형 생성 에러")
+    void generate_triangle_under_three_points() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        List<Point> twoPoints = Lists.newArrayList(point1, point2);
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(twoPoints));
+    }
+
+    @Test
+    @DisplayName("3개 초과하는 점들로 삼각형 생성 에러")
+    void generate_triangle_over_three_points() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point3 = Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point4 = Point.of(MAX_X_COORDINATE, MIN_Y_COORDINATE);
+        List<Point> fourPoints = Lists.newArrayList(point1, point2, point3, point4);
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(fourPoints));
+    }
+
+    @Test
+    @DisplayName("같은 점을 포함하는 세 점을 입력하는 경우 삼각형 생성 에러")
+    void generate_triangle_with_same_points() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        List<Point> points = Lists.newArrayList(point1, point2, point2);
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(points));
+    }
+
+    @Test
+    @DisplayName("같은 기울기의 점들을 입력하는 경우 (한 직선에 세 점이 있는 경우) 삼각형 생성 에러")
+    void generate_triangle_with_same_slopes() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point3 = Point.of(MIN_X_COORDINATE + 1, MIN_Y_COORDINATE + 1);
+        List<Point> points = Lists.newArrayList(point1, point2, point3);
+
+        assertThrows(IllegalArgumentException.class, () -> new Triangle(points));
+    }
+
+    @Test
+    @DisplayName("점을 포함하는지 테스트")
+    void contains_point() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point3 = Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE);
+        List<Point> points = Lists.newArrayList(point1, point2, point3);
+
+        Triangle triangle = assertDoesNotThrow(() -> new Triangle(points));
+        assertTrue(triangle.contains(point1));
+        assertTrue(triangle.contains(point2));
+        assertTrue(triangle.contains(point3));
+    }
+
+    @Test
+    @DisplayName("같은 점들로 구성된 삼각형들이 같은 객체인지")
+    void equals() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point3 = Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE);
+        List<Point> points = Lists.newArrayList(point1, point2, point3);
+
+        Triangle triangle1 = assertDoesNotThrow(() -> new Triangle(points));
+        Triangle triangle2 = assertDoesNotThrow(() -> new Triangle(points));
+        assertEquals(triangle1, triangle2);
+    }
+
+    @Test
+    @DisplayName("같은 꼭지점의 삼각형들을 HashSet에 넣을 경우 중복으로 인식하는지")
+    void equal_triangles_in_hashset_size_1() {
+        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
+        Point point3 = Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE);
+        List<Point> points = Lists.newArrayList(point1, point2, point3);
+
+        Triangle triangle1 = assertDoesNotThrow(() -> new Triangle(points));
+        Triangle triangle2 = assertDoesNotThrow(() -> new Triangle(points));
+        assertThat(Sets.newHashSet(Lists.newArrayList(triangle1, triangle2)).size()).isEqualTo(1);
+    }
+}
