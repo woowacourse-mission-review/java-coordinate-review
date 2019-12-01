9d0f679d7265dfbda188e5f978828d2d3abe1663
DaejunBae <dpudpu11@gmail.com>
2019-12-01 12:28:36 +0900

    style: PointGroupFactory 상수 순서 변경

--- src/main/java/coordinate/domain/point/PointGroupFactory.java
+++ src/main/java/coordinate/domain/point/PointGroupFactory.java
@@ -8,16 +8,16 @@ import java.util.stream.Stream;
 public class PointGroupFactory {
     static final String NOT_VALID_INPUT_EXCEPTION_MESSAGE = "유효한 입력이 아닙니다. ex) (10,10)-(13,10)-(20,8)";
     private static final String REGEX = "(\\([0-9]{1,2},[0-9]{1,2}\\)-)+\\([0-9]{1,2},[0-9]{1,2}\\)";
-    private static final Pattern VALID_INPUT_PATTERN = Pattern.compile(REGEX);
     private static final String DELIMITER_POINT = "-";
     private static final String DELIMITER_COORDINATE = ",";
+    private static final Pattern VALID_INPUT_PATTERN = Pattern.compile(REGEX);
 
     public PointGroup create(final String inputPoints) {
         validateInput(inputPoints);
 
         final List<Point> points = Stream.of(inputPoints.split(DELIMITER_POINT))
                 .map(s -> s.substring(1, s.length() - 1)) // (1,2) 에서 () 제거
-                .map(point -> { // 1,2 에서 ,제거
+                .map(point -> {                           // 1,2 에서 ,제거 후 Point 생성
                     int index = point.indexOf(DELIMITER_COORDINATE);
                     int x = Integer.parseInt(point.substring(0, index));
                     int y = Integer.parseInt(point.substring(index + 1));
