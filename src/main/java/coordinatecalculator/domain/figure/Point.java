1c1816ad8e72e7c46fbca8abea98c6df21211e95
skygl <jupiter9501@gmail.com>
2019-12-01 13:15:31 +0900

    feat: Point 클래스 추가

---
+++ src/main/java/coordinatecalculator/domain/figure/Point.java
@@ -0,0 +1,35 @@
+package coordinatecalculator.domain.figure;
+
+import coordinatecalculator.domain.coordinates.XCoordinate;
+import coordinatecalculator.domain.coordinates.YCoordinate;
+
+import java.util.Objects;
+
+public class Point {
+
+    private final XCoordinate xCoordinate;
+    private final YCoordinate yCoordinate;
+
+    private Point(final XCoordinate xCoordinate, final YCoordinate yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public static Point of(final int xCoordinate, final int yCoordinate) {
+        return new Point(XCoordinate.of(xCoordinate), YCoordinate.of(yCoordinate));
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Point point = (Point) o;
+        return xCoordinate.equals(point.xCoordinate) &&
+                yCoordinate.equals(point.yCoordinate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoordinate, yCoordinate);
+    }
+}
---
+++ src/test/java/coordinatecalculator/domain/figure/PointTest.java
@@ -0,0 +1,58 @@
+package coordinatecalculator.domain.figure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static coordinatecalculator.domain.coordinates.XCoordinate.MAX_X_COORDINATE;
+import static coordinatecalculator.domain.coordinates.XCoordinate.MIN_X_COORDINATE;
+import static coordinatecalculator.domain.coordinates.YCoordinate.MAX_Y_COORDINATE;
+import static coordinatecalculator.domain.coordinates.YCoordinate.MIN_Y_COORDINATE;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    @DisplayName("Point 생성")
+    void generate_point() {
+        assertDoesNotThrow(() -> Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE));
+        assertDoesNotThrow(() -> Point.of(MAX_X_COORDINATE, MIN_Y_COORDINATE));
+        assertDoesNotThrow(() -> Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE));
+        assertDoesNotThrow(() -> Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE));
+    }
+
+    @Test
+    @DisplayName("X 좌표가 최솟값보다 작은 경우 에러")
+    void generate_point_under_min_x_coordinate() {
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MIN_X_COORDINATE - 1, MIN_Y_COORDINATE));
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MIN_X_COORDINATE - 1, MAX_Y_COORDINATE));
+    }
+
+    @Test
+    @DisplayName("X 좌표가 최댓값보다 큰 경우 에러")
+    void generate_point_over_max_x_coordinate() {
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MAX_X_COORDINATE + 1, MIN_Y_COORDINATE));
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MAX_X_COORDINATE + 1, MAX_Y_COORDINATE));
+    }
+
+    @Test
+    @DisplayName("Y 좌표가 최솟값보다 작은 경우 에러")
+    void generate_point_under_min_y_coordinate() {
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE - 1));
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MAX_X_COORDINATE, MIN_Y_COORDINATE - 1));
+    }
+
+    @Test
+    @DisplayName("Y 좌표가 최댓값보다 큰 경우 에러")
+    void generate_point_over_max_y_coordinate() {
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE + 1));
+        assertThrows(IllegalArgumentException.class, () -> Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE + 1));
+    }
+
+    @Test
+    @DisplayName("같은 좌표의 Point 간 객체 비교")
+    void equals() {
+        int xCoordinate = 15;
+        int yCoordinate = 15;
+        assertEquals(Point.of(xCoordinate, yCoordinate), Point.of(xCoordinate, yCoordinate));
+    }
+}
