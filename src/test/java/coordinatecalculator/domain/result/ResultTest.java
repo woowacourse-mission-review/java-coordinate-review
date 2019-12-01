354278a23fa403e77f0a3204f1fef5a26aa4e6cd
skygl <jupiter9501@gmail.com>
2019-12-01 22:39:25 +0900

    test: 테스트 추가

--- src/test/java/coordinatecalculator/domain/result/ResultTest.java
+++ src/test/java/coordinatecalculator/domain/result/ResultTest.java
@@ -11,7 +11,8 @@ import java.util.List;
 import static coordinatecalculator.domain.coordinates.XCoordinate.MAX_X_COORDINATE;
 import static coordinatecalculator.domain.coordinates.XCoordinate.MIN_X_COORDINATE;
 import static coordinatecalculator.domain.coordinates.YCoordinate.MAX_Y_COORDINATE;
-import static coordinatecalculator.domain.coordinates.YCoordinate.MIN_Y_COORDINATE;
+import static coordinatecalculator.domain.figure.line.LineLengthAttributeCreator.LINE_LENGTH_ATTRIBUTES_MESSAGE_FORMAT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ResultTest {
@@ -19,7 +20,7 @@ class ResultTest {
     @Test
     @DisplayName("직선 결과")
     void line_result() {
-        Point point1 = Point.of(MIN_X_COORDINATE, MIN_Y_COORDINATE);
+        Point point1 = Point.of(MIN_X_COORDINATE, MAX_Y_COORDINATE);
         Point point2 = Point.of(MAX_X_COORDINATE, MAX_Y_COORDINATE);
         List<Point> points = Lists.newArrayList(point1, point2);
         Line line = Line.of(points);
@@ -27,5 +28,7 @@ class ResultTest {
         Result result = line.createResult();
         assertTrue(result.contains(point1));
         assertTrue(result.contains(point2));
+        double length = MAX_X_COORDINATE - MIN_X_COORDINATE;
+        assertEquals(result.getAttributeMessages().get(0), String.format(LINE_LENGTH_ATTRIBUTES_MESSAGE_FORMAT, length));
     }
 }
